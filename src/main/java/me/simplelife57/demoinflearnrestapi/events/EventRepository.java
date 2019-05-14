c8f8cf8660b5c72f4c283e84fd67e1acd083dfba
simplelife57 <jiwoo627@naver.com>
2019-05-15 00:04:31 +0900

    POST /api/events returns 201

---
+++ src/main/java/me/simplelife57/demoinflearnrestapi/events/EventRepository.java
@@ -0,0 +1,6 @@
+package me.simplelife57.demoinflearnrestapi.events;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Integer> {
+}
