f15dd5e4c335da537638f5df3ef8e1eacf783fe6
simplelife57 <jiwoo627@naver.com>
2019-05-15 01:20:27 +0900

    Ristrict input for POST /api/events

--- src/main/java/me/simplelife57/demoinflearnrestapi/events/EventController.java
+++ src/main/java/me/simplelife57/demoinflearnrestapi/events/EventController.java
@@ -1,9 +1,11 @@
 package me.simplelife57.demoinflearnrestapi.events;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +21,16 @@ public class EventController {
 
 	private final EventRepository eventRepository;
 
-	public EventController(EventRepository eventRepository) {
+	private final ModelMapper modelMapper;
+
+	public EventController(EventRepository eventRepository, ModelMapper modelMapper) {
 		this.eventRepository = eventRepository;
+		this.modelMapper = modelMapper;
 	}
 
 	@PostMapping
-	public ResponseEntity createEvent(@RequestBody Event event) {
+	public ResponseEntity createEvent(@RequestBody EventDto eventDto) {
+		Event event =  modelMapper.map(eventDto, Event.class);
 		Event newEvent = this.eventRepository.save(event);
 		URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
 		return ResponseEntity.created(createdUri).body(event);
--- src/test/java/me/simplelife57/demoinflearnrestapi/events/EventControllerTest.java
+++ src/test/java/me/simplelife57/demoinflearnrestapi/events/EventControllerTest.java
@@ -1,11 +1,14 @@
 package me.simplelife57.demoinflearnrestapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
@@ -21,7 +24,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class EventControllerTest {
 
 	@Autowired
@@ -30,12 +34,10 @@ public class EventControllerTest {
 	@Autowired
 	ObjectMapper objectMapper;
 
-	@MockBean
-	EventRepository eventRepository;
-
 	@Test
 	public void createEvent() throws Exception {
 		Event event = Event.builder()
+				.id(100)
 				.name("Spring")
 				.description("REST API Development with Spring")
 				.beginEnrollmentDateTime(LocalDateTime.of(2018, 11, 23, 14, 14, 21))
@@ -46,9 +48,10 @@ public class EventControllerTest {
 				.maxPrice(200)
 				.limitOfEnrollment(100)
 				.location("강남역 D2 스타텁 팩토리")
+				.free(true)
+				.offline(false)
+				.eventStatus(EventStatus.PUBLISHED)
 				.build();
-		event.setId(10);
-		Mockito.when(eventRepository.save(event)).thenReturn(event);
 
 		mockMvc.perform(post("/api/events/")
 				.contentType(MediaType.APPLICATION_JSON_UTF8)
@@ -58,6 +61,9 @@ public class EventControllerTest {
 			.andExpect(status().isCreated())
 			.andExpect(jsonPath("id").exists())
 			.andExpect(header().exists(HttpHeaders.LOCATION))
-			.andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE));
+			.andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
+			.andExpect(jsonPath("id").value(Matchers.not(100)))
+			.andExpect(jsonPath("free").value(Matchers.not("true")))
+			.andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
 	}
 }
