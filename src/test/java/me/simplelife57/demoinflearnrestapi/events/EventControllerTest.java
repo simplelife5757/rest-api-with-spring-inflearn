664d89acc3bc2733d51149ed30d64a73e6179878
simplelife57 <jiwoo627@naver.com>
2019-05-15 21:13:03 +0900

    fail on unknown properties

--- src/test/java/me/simplelife57/demoinflearnrestapi/events/EventControllerTest.java
+++ src/test/java/me/simplelife57/demoinflearnrestapi/events/EventControllerTest.java
@@ -36,6 +36,35 @@ public class EventControllerTest {
 
 	@Test
 	public void createEvent() throws Exception {
+		EventDto event = EventDto.builder()
+				.name("Spring")
+				.description("REST API Development with Spring")
+				.beginEnrollmentDateTime(LocalDateTime.of(2018, 11, 23, 14, 14, 21))
+				.closeEnrollmentDateTime(LocalDateTime.of(2018, 11, 24, 14, 14, 21))
+				.beginEventDateTime(LocalDateTime.of(2018, 11, 25, 14, 14, 21))
+				.endEventDateTime(LocalDateTime.of(2018, 11, 26, 14, 14, 21))
+				.basePrice(100)
+				.maxPrice(200)
+				.limitOfEnrollment(100)
+				.location("강남역 D2 스타텁 팩토리")
+				.build();
+
+		mockMvc.perform(post("/api/events/")
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.accept(MediaTypes.HAL_JSON)
+				.content(objectMapper.writeValueAsString(event)))
+			.andDo(print())
+			.andExpect(status().isCreated())
+			.andExpect(jsonPath("id").exists())
+			.andExpect(header().exists(HttpHeaders.LOCATION))
+			.andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
+			.andExpect(jsonPath("id").value(Matchers.not(100)))
+			.andExpect(jsonPath("free").value(Matchers.not("true")))
+			.andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+	}
+
+	@Test
+	public void createEvent_Bad_Request() throws Exception {
 		Event event = Event.builder()
 				.id(100)
 				.name("Spring")
@@ -57,13 +86,7 @@ public class EventControllerTest {
 				.contentType(MediaType.APPLICATION_JSON_UTF8)
 				.accept(MediaTypes.HAL_JSON)
 				.content(objectMapper.writeValueAsString(event)))
-			.andDo(print())
-			.andExpect(status().isCreated())
-			.andExpect(jsonPath("id").exists())
-			.andExpect(header().exists(HttpHeaders.LOCATION))
-			.andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
-			.andExpect(jsonPath("id").value(Matchers.not(100)))
-			.andExpect(jsonPath("free").value(Matchers.not("true")))
-			.andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+				.andDo(print())
+				.andExpect(status().isBadRequest());
 	}
 }
